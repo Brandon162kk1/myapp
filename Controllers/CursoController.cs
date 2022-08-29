8056a051b05cb73f460c37ef58ecb5f754fae1ad
Brandon.Rodriguez <brandon162001@gmail.com>
2022-08-28 22:01:36 -0500

    Cambios

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -26,52 +26,41 @@ namespace myapp.Controllers
 
         public IActionResult Registrar(Curso objCurso)
         {
-        int v1,v2,v3,cred=3 , preCred=100,monto;
+
+        int v1,v2,v3,cred=3,preCred=100,monto;
         double igv,mt;
+
         v1=Convert.ToInt32(objCurso.Materia1);
         v2=Convert.ToInt32(objCurso.Materia2);
         v3=Convert.ToInt32(objCurso.Materia3);
+
         if(v1==1 && v2==1 && v3==1){
             monto=cred*preCred*3;
             igv=0.18*monto;
-            mt=monto+igv;
-            ViewData["Message1"] ="Total Cursos: " + monto;
-            ViewData["Message2"] ="IGV: " + igv;
-            ViewData["Message3"] ="Total a Pagar: " + mt;
-            return View("Index"); 
+            mt=monto+igv; 
         }else if(v1==1 && v2==1){
             monto=cred*preCred*2;
             igv=0.18*monto;
-            mt=monto+igv;
-            ViewData["Message1"] ="Total Cursos: " + monto;
-            ViewData["Message2"] ="IGV: " + igv;
-            ViewData["Message3"] ="Total a Pagar: " + mt;
-            return View("Index");  
+            mt=monto+igv;  
         }else if(v1==1 && v3==1){
             monto=cred*preCred*2;
             igv=0.18*monto;
             mt=monto+igv;
-            ViewData["Message1"] ="Total Cursos: " + monto;
-            ViewData["Message2"] ="IGV: " + igv;
-            ViewData["Message3"] ="Total a Pagar: " + mt;
-            return View("Index");  
         }else if(v2==1 && v3==1){
             monto=cred*preCred*2;
             igv=0.18*monto;
-            mt=monto+igv;
-            ViewData["Message1"] ="Total Cursos: " + monto;
-            ViewData["Message2"] ="IGV: " + igv;
-            ViewData["Message3"] ="Total a Pagar: " + mt;
-            return View("Index");  
+            mt=monto+igv;;  
         }else{
             monto=cred*preCred;
             igv=0.18*monto;
-            mt=monto+igv;
-            ViewData["Message1"] ="Total Cursos: " + monto;
-            ViewData["Message2"] ="IGV: " + igv;
-            ViewData["Message3"] ="Total a Pagar: " + mt;
-            return View("Index");  
-        } 
+            mt=monto+igv;  
+        }
+
+        ViewData["Message1"] ="Total Cursos: " + monto;
+        ViewData["Message2"] ="IGV: " + igv;
+        ViewData["Message3"] ="Total a Pagar: " + mt;
+        return View("Index");
+        
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
